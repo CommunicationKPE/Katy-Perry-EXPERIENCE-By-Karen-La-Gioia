56a383921207837c14c84c7a8f37285db4423030
EtiennePONS <etienne.pons@hotmail.fr>
2025-11-27 20:35:03 +0100

    footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,28 +2,13 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      <div className="footer">
-        <div className="footer-top">
-          <div className="footer-top-left">
-            {/* <img src={footer_logo} alt="" /> */}
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-              voluptatum.
-            </p>
-          </div>
-        </div>
-        <hr />
-        <div className="footer-bottom">
-          <p className="footer-bottom-left">
-            &copy; 2024 Your Company. All rights reserved.
-          </p>
-          <div className="footer-bottom-right">
-            <p>Term of Services</p>
-            <p>Privacy Policy</p>
-          </div>
-        </div>
-      </div>
+    <div class="copyright">
+      <p>
+        KatyPerryEXPERIENCEbyKarenLaGioia / WebApp / <br></br>
+        <i class="fas fa-drum"></i>
+        Developed by ///Atn/// With
+        <i class="far fa-heart"></i>
+      </p>
     </div>
   );
 };
