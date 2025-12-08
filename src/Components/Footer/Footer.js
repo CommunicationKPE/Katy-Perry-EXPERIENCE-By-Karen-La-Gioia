490d1295e00cb576ac454ef454f6a8d7db1d32c1
EtiennePONS <etienne.pons@hotmail.fr>
2025-12-08 11:23:41 +0100

    footer avec bonne écriture

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,13 @@
 import "./Footer.css";
 
 const Footer = ({ visits }) => {
-  console.log(visits);
+  // console.log(visits);
   return (
     <div className="copyright">
       <p>
-        KatyPerryEXPERIENCEbyKarenLaGioia<br></br> / WebApp / {visits} visiteurs
+        KatyPerryEXPERIENCEbyKarenLaGioia<br></br> / WebApp / MERCI vous êtes le{" "}
+        {visits}
+        <sup>e</sup> visiteurs
         <br></br>
         <i className="fas fa-drum"></i>
         -Developed by ///Atn/// With-
