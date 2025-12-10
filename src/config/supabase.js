7b86b4282646628c27995a5f87b09ee9571bc768
EtiennePONS <etienne.pons@hotmail.fr>
2025-12-10 15:28:52 +0100

    variables d'environement dans .env

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -2,15 +2,13 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
-const serviceID = "service_dn2i078";
-const templateFromAPP = "template_0ktsajm";
-const templateAutoReply = "template_yfs6d2a";
-const publicKey = "udrPtWt6rj3Nf0ltC";
+const serviceID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+const templateFromAPP = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+const publicKey = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 export const emailjs = {
   serviceID,
   templateFromAPP,
-  templateAutoReply,
   publicKey,
 };
